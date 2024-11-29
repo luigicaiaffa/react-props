69527c291e515931f4d1e60d1f8db76285ac1da8
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-29 16:29:34 +0100

    add badge color

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,3 +1,22 @@
 export default function Badge({ tag }) {
-  return <span className="tags-badge text-bg-success rounded p-1">{tag}</span>;
+  let tagColor;
+
+  if (tag === "html") {
+    tagColor = "text-bg-success";
+  }
+  if (tag === "css") {
+    tagColor = "text-bg-primary";
+  }
+  if (tag === "js") {
+    tagColor = "bg-warning text-white";
+  }
+  if (tag === "php") {
+    tagColor = "text-bg-danger";
+  }
+
+  return (
+    <span className={tagColor + " rounded" + " p-1" + " tags-badge"}>
+      &#35;{tag}
+    </span>
+  );
 }
