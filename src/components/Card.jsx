87062daaacb4beea000aca59cba83ddab56df546
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-29 15:48:29 +0100

    add stamp filtered posts

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,18 @@
 import img from "../assets/img/placeholder-600x400.png";
 import Button from "./Button";
 
-export default function Card(props) {
+export default function Card({ id, title, image, content, tags }) {
   return (
     <div className="col-12 col-md-6 col-lg-4">
       <div className="card card-main">
-        <img src={img} className="card-img-top" alt="img" />
+        <img src={image || img} className="card-img-top" alt="img" />
         <div className="card-body">
-          <h5 className="card-title">Titolo del Post</h5>
-          <p className="card-text">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
-            reiciendis, velit dolorem iure recusandae assumenda iste deserunt
-            nulla rerum hic quam commodi corrupti, expedita modi! Ullam, harum?
-          </p>
-          <Button style="card-btn">LEGGI DI PI&#217;</Button>
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{content}</p>
+          <p>post n.{id}</p>
+          <div className="d-flex justify-content-between">
+            <Button style="card-btn">LEGGI DI PI&#217;</Button>
+          </div>
         </div>
       </div>
     </div>
