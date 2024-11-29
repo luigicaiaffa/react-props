6cff8c3ed74a0cc3756553cbe39069fd0a8ad5e5
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-29 15:40:03 +0100

    add card props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,27 @@
 import Card from "./Card";
+import { posts } from "../data/posts";
 
 export default function Main() {
+  const filteredPosts = posts.filter((post) => post.published === true);
+
   return (
     <main>
       <div className="container mb-5">
-        <section className="d-flex justify-content-center">
-          <Card></Card>
+        <section>
+          <div className="row g-5 justify-content-center">
+            {filteredPosts.map((post) => {
+              return (
+                <Card
+                  key={post.id}
+                  id={post.id}
+                  title={post.title}
+                  image={post.image}
+                  content={post.content}
+                  tags={post.tags}
+                />
+              );
+            })}
+          </div>
         </section>
       </div>
     </main>
